b8a83c9a067d8f87ff632bad350f34b04edac580
Julian Doucette <j1d@hush.ai>
2013-07-23 17:10:39 -0300

    Working on / testing new output formats.

--- archive-official.php
+++ archive-official.php
@@ -1,93 +1,103 @@
 <?
-
 global $wp_query;
 
 $hat_officials_pagination = array (
-    'base' => str_replace (9999, '%#%', esc_url (get_pagenum_link (9999))),
-    'format' => '?paged=%#%',
-    'current' => max(1, get_query_var('paged')),
-    'total' => $wp_query->max_num_pages,
-    'type' => 'list'
+	'base' => str_replace (9999, '%#%', esc_url (get_pagenum_link (9999))),
+	'format' => '?paged=%#%',
+	'current' => max (1, get_query_var ('paged')),
+	'total' => $wp_query->max_num_pages,
+	'type' => 'list' 
 );
 
 ?>
 <? get_header (); ?>
 <section>
 
-    <form id="filter-officials" method="get" action="<?= esc_url (get_post_type_archive_link ('official')); ?>">
-      <ul>
-        <li>
-          <label for="official_first_name"><? _e('First Name', 'hat'); ?></label>
-          <input name="official_first_name" type="text" placeholder="<? _e('First Name', 'hat'); ?>" />
-        </li>
-        <li>
-          <label for="official_last_name"><? _e('Last Name','hat'); ?></label>
-          <input name="official_last_name" type="text" placeholder="<? _e('Last Name', 'hat'); ?>" />
-        </li>
-        <li>
-          <label for="official_title"><? _e('Office Title', 'hat'); ?></label>
-          <input name="official_title" type="text" placeholder="<? _e('Official Title', 'hat'); ?>" />
-        </li>
-        <li>
-          <label for="official_id_office_state"><? _e('Office State', 'hat'); ?></label>
-          <input name="official_id_office_state" type="text" placeholder="<? _e('Office State', 'hat'); ?>" />
-        </li>
-        <li>
-          <label for="official_office_city"><? _e('Office City', 'hat'); ?></label>
-          <input name="official_office_city" type="text" placeholder="<? _e('Office City', 'hat'); ?>" />
-        </li>
-        <li>
-          <label for="official_district"><? _e('Official District', 'hat'); ?></label>
-          <input name="official_district" type="text" placeholder="<? _e('Official District', 'hat'); ?>" />
-        </li>
-        <li>
-          <input type="hidden" name="post_type" value="official" />
-          <button name="official_filter" value="1" type="submit">Filter Results</button>
-        </li>
-      </ul>
-    </form>
+	<form id="filter-officials" method="get" action="<?= esc_url (get_post_type_archive_link ('official')); ?>">
+		<ul>
+			<li>
+				<label for="official_first_name">
+					<? _e('First Name', 'hat'); ?>
+				</label>
+				<input name="official_first_name" type="text"
+				placeholder="<? _e('First Name', 'hat'); ?>" />
+			</li>
+			<li>
+				<label for="official_last_name">
+					<? _e('Last Name','hat'); ?>
+				</label>
+				<input name="official_last_name" type="text"
+				placeholder="<? _e('Last Name', 'hat'); ?>" />
+			</li>
+			<li>
+				<label for="official_title">
+					<? _e('Office Title', 'hat'); ?>
+				</label>
+				<input name="official_title" type="text"
+				placeholder="<? _e('Official Title', 'hat'); ?>" />
+			</li>
+			<li>
+				<label for="official_id_office_state">
+					<? _e('Office State', 'hat'); ?>
+				</label>
+				<input name="official_id_office_state" type="text"
+				placeholder="<? _e('Office State', 'hat'); ?>" />
+			</li>
+			<li>
+				<label for="official_office_city">
+					<? _e('Office City', 'hat'); ?>
+				</label>
+				<input name="official_office_city" type="text"
+				placeholder="<? _e('Office City', 'hat'); ?>" />
+			</li>
+			<li>
+				<label for="official_district">
+					<? _e('Official District', 'hat'); ?>
+				</label>
+				<input name="official_district" type="text"
+				placeholder="<? _e('Official District', 'hat'); ?>" />
+			</li>
+			<li>
+				<input type="hidden" name="post_type" value="official" />
+				<button name="official_filter" value="1" type="submit">
+					Filter Results
+				</button>
+			</li>
+		</ul>
+	</form>
 
-    <table id="officials" class="table table-hover table-condensed">
-      <thead>
-        <tr>
-          <td><? _e('#', 'hat'); ?></td>
-          <td><? _e('First Name', 'hat'); ?></td>
-          <td><? _e('Last Name', 'hat'); ?></td>
-          <td><? _e('Title', 'hat'); ?></td>
-          <td><? _e('State', 'hat'); ?></td>
-          <td><? _e('City', 'hat'); ?></td>
-          <td><? _e('District', 'hat'); ?></td>
-        </tr>
-      </thead>
-      <tbody>
-      <? while (have_posts ()): the_post (); ?>
-        <tr>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_support', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_first_name', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_last_name', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_title', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_id_office_state', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_office_city', true); ?></a></td>
-          <td><a href="<? the_permalink (); ?>" title="<? the_title (); ?>"><?= get_post_meta (get_the_ID (), 'official_district', true); ?></a></td>
-        </tr>
-      <? endwhile; ?>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td><? _e('#', 'hat'); ?></td>
-          <td><? _e('First Name', 'hat'); ?></td>
-          <td><? _e('Last Name', 'hat'); ?></td>
-          <td><? _e('Official Title', 'hat'); ?></td>
-          <td><? _e('Office State', 'hat'); ?></td>
-          <td><? _e('Office City', 'hat'); ?></td>
-          <td><? _e('District', 'hat'); ?></td>
-        </tr>
-      </tfoot>
-    </table>
+	<table id="officials" class="table table-hover table-condensed">
+		<thead>
+			<tr>
+				<td><i class="icon-th"></i></td>
+				<td><? _e ('Official Profile','hat'); ?></td>
+				<td><? _e ('Contact Information','hat'); ?></td>
+				<td><? _e ('Official Support', 'hat'); ?></td>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td><i class="icon-th"></i></td>
+				<td><? _e ('Official Profile','hat'); ?></td>
+				<td><? _e ('Contact Information','hat'); ?></td>
+				<td><? _e ('Official Support', 'hat'); ?></td>
+			</tr>
+		</tfoot>
+		<tbody>
+		<? while (have_posts ()): the_post (); ?>
+			<tr>
+				<td>Ranking</td>
+				<td>Profile</td>
+				<td>Contact</td>
+				<td>Support</td>
+			</tr>
+		<? endwhile; ?>
+		</tbody>
+	</table>
 
-    <div class="pagination">
-      <?= paginate_links ($hat_officials_pagination); ?>
-    </div>
+	<div class="pagination">
+		<?= paginate_links ($hat_officials_pagination); ?>
+	</div>
 
 </section>
 <? get_sidebar (); ?>
--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,77 @@
 <?
 
+function get_official_name ($id = NULL)
+{
+	if (is_null ($id)) {
+		$id = get_the_ID ();
+	}
+	return get_post_meta ($id, Official::META_NAME_FIRST, true) . ' '
+		. get_post_meta ($id, Official::META_NAME_LAST, true);
+}
+
+function the_official_name ()
+{
+	echo get_official_name();
+}
+
+function get_official_title ($id = NULL)
+{
+	if (is_null ($id)) {
+		$id = get_the_ID ();
+	}
+	return get_post_meta ($id, Official::META_OFFICE_TITLE, true);
+}
+
+function the_official_title ()
+{
+	echo get_official_title();
+}
+
+function get_official_contact ($id = NULL)
+{
+	if (is_null ($id)) {
+		$id = get_the_ID ();
+	}
+	return array (
+		__('Office Phone 1', 'hat') => get_post_meta ($id, Official::META_OFFICE_PHONE_1, true),
+		__('Office Phone 2', 'hat') => get_post_meta ($id, Official::META_OFFICE_PHONE_2, true),
+		__('District Phone 1', 'hat') => get_post_meta ($id, Official::META_DISTRICT_PHONE_1, true),
+		__('District Phone 2', 'hat') => get_post_meta ($id, Official::META_DISTRICT_PHONE_2, true)
+	);
+}
+
+function the_official_contact ()
+{
+	$contact = get_official_contact();
+	?>
+	<ul>
+	<? foreach ($contact as $key => $value): ?>
+	<? if ($value != ""): ?>
+		<li>
+			<a href="tel:<?= $value; ?>" title="<?= $key; ?>"><?= $value; ?></a>
+		</li>
+	<? endif; ?>
+	<? endforeach; ?>
+	</ul>
+	<?
+}
+
+
+function the_support_rating ()
+{
+
+}
+
+function the_support_indicator ()
+{
+
+}
+
+function the_support_ranking ()
+{
+	
+}
+
 function the_official_titles ()
 {
 
